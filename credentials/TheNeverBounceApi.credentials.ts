cf62825d96ae5edf316dbd1965c9b2d4170f885d
NeehalAhmedQureshi <m.nehalq125@gmail.com>
2025-09-03 13:26:56 +0500

    chore(build): update dist output

--- credentials/TheNeverBounceApi.credentials.ts
+++ credentials/TheNeverBounceApi.credentials.ts
@@ -5,7 +5,7 @@ import {
 } from 'n8n-workflow';
 
 export class TheNeverBounceApi implements ICredentialType {
-	name = 'theNeverBounceApiToken';
+	name = 'theNeverBounceApi';
 displayName = 'NeverBounce API';
 	// Uses the link to this tutorial as an example
 	// Replace with your own docs links when building your own nodes
